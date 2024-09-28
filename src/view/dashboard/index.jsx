f7711d68d5d6dcdd7de16e69be32f59156b7b924
amr88nzzal <amr.nzzal@gmail.com>
2024-09-28 21:54:47 +0200

    Dashboard

---
+++ src/view/dashboard/index.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import Header from "../../component/Header";
+
+export default function Dashboard() {
+  return (
+    <Box>
+      <Header title="DASHBOARD" subtitle="WELCOME" />
+    </Box>
+  );
+}
